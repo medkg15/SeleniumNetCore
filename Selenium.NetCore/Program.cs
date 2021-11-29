a435113085f77f5b6bfcfad05e342e57ba9bb3ea
kgr@alpha-solutions.us <kgr@alpha-solutions.us>
2021-11-29 15:04:33 -0500

    Switch to sending bstack:options for Selenium 4 based on https://gist.github.com/HeenaNanwani8/97d8ac4725731fb7a2e67a5f2f6b3499

--- Selenium.NetCore/Program.cs
+++ Selenium.NetCore/Program.cs
@@ -3,6 +3,9 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace Selenium.NetCore
 {
@@ -16,31 +19,44 @@ namespace Selenium.NetCore
             var localArgs = new List<KeyValuePair<string, string>>() {
                 new KeyValuePair<string, string>("key", args[1]),
                 new KeyValuePair<string, string>("v", "true"),
+                new KeyValuePair<string, string>("force", "true"),
             };
 
             local.start(localArgs);
 
-            // doesn't appear this will ever succeed:
-            //while (!local.isRunning())
-            //{
-            //    Thread.Sleep(1000);
-            //}
+            while (!Process.GetProcessesByName("BrowserStackLocal").Any())
+            {
+                Thread.Sleep(1000);
+            }
 
-            OpenQA.Selenium.Chrome.ChromeOptions options = new OpenQA.Selenium.Chrome.ChromeOptions();
-            options.AddAdditionalCapability("browserstack.local", "true");
-            options.AddAdditionalCapability("os_version", "10", true);
-            options.AddAdditionalCapability("browser", "chrome", true);
-            options.AddAdditionalCapability("browser_version", "latest", true);
-            options.AddAdditionalCapability("os", "Windows", true);
+            var options = new OpenQA.Selenium.Chrome.ChromeOptions();
+            options.BrowserVersion = "latest";
+
+            var browserstackOptions = new Dictionary<string, object>
+            {
+                { "os", "Windows" },
+                { "osVersion", "10" },
+                { "local", "true" },
+                { "userName", args[0] },
+                { "accessKey", args[1] }
+            };
+            options.AddAdditionalOption("bstack:options", browserstackOptions);
 
             var driver = new RemoteWebDriver(
-              new Uri($"{args[0]}:{args[1]}@hub-cloud.browserstack.com/wd/hub/"),
+              new Uri($"https://hub-cloud.browserstack.com/wd/hub/"),
               options
             );
 
-            driver.Navigate().GoToUrl("http://localhost");
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(localDriver => localDriver.Title.ToLower().Contains("sample"));
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost");
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(localDriver => localDriver.Title.ToLower().Contains("Sitecore Active Directory Login"));
+            }
+            finally
+            {
+                driver.Dispose();
+            }
         }
     }
 }
